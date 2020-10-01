e46c039580e0fd43ca267bb49f2d91b876a836b8
sunilsahu000 <72176852+sunilsahu000@users.noreply.github.com>
2020-10-01 20:31:06 +0530

    User intiger input has been changed to exa Integer.valueOf (readLine ())

--- src/Set.kt
+++ src/Set.kt
@@ -1,9 +1,9 @@
 fun main(args: Array<String>) {
     val arr = arrayListOf<Int>()
     //var ar = arrayListOf<Int>()
-    var a = readLine()!!.toInt()
+    var a =Integer.valueOf(readLine())
     do {
-        val input = readLine()!!.toInt()
+        val input =Integer.valueOf(readLine())
         arr.add(input)
     } while (arr.size <= (a - 1))
     println(arr)
